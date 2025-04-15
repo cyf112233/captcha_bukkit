543551df3ba82904c44df6c08379c6cda7ed3ef5
cyf112233 <122795125+cyf112233@users.noreply.github.com>
2025-04-15 21:13:25 +0800

    Delete src/main directory

--- src/main/java/byd/cxkcxkckx/captcha/func/AbstractModule.java
+++
@@ -1,9 +0,0 @@
-package byd.cxkcxkckx.captcha.func;
-        
-import byd.cxkcxkckx.captcha.captcha;
-
-public abstract class AbstractModule extends top.mrxiaom.pluginbase.func.AbstractModule<captcha> {
-    public AbstractModule(captcha plugin) {
-        super(plugin);
-    }
-}
